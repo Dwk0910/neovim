8d0e0b7028fc068d76f2fefe314df7290d08725a
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-23 22:25:37 +0900

    Feature: Stability increased

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,11 +3,18 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
-
 		configs.setup({
 			ensure_installed = { "lua", "java", "javascript", "typescript", "html", "css" },
 			sync_install = false,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				disable = function(lang, buf)
+					local ok, _ = pcall(function()
+						return vim.api.nvim_buf_line_count(buf) > 1000
+					end)
+					return not ok
+				end,
+			},
 			indent = { enable = true },
 		})
 	end,
