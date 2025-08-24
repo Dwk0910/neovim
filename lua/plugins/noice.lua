0a9b4a31d7b2deb6eee45aa782a15da79f3e96b4
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-25 06:35:48 +0900

    Fix: notice commandline doen't have margin of top

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,17 +1,45 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = function(_, opts)
-		opts.routes = opts.routes or {}
-
-		table.insert(opts.routes, {
-			filter = {
-				event = "notify",
-				find = "No information available",
+	config = function()
+		require("noice").setup({
+			routes = {
+				{
+					filter = {
+						event = "notify",
+						find = "No information available",
+					},
+					opts = { skip = true },
+				},
 			},
-			opts = { skip = true },
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			presets = {
+				bottom_search = false, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
+			views = {
+				cmdline_popup = {
+					position = {
+						row = 5,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+			},
 		})
-
 		-- Notify plugin default settings
 		vim.cmd([[
             highlight NotifyERRORBorder guifg=#8A1F1F
@@ -36,6 +64,7 @@ return {
             highlight link NotifyTRACEBody Normal
         ]])
 	end,
+
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		{
@@ -45,5 +74,6 @@ return {
 			},
 		},
 	},
+
 	enabled = true,
 }
