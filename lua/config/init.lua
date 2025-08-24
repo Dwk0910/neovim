c2d2819590b986491973a95dfdb7aaf0fff30e26
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-24 14:26:06 +0900

    Feature: Added diagnostic icons from nerd-icons

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,5 @@
+vim.env.PATH = vim.env.PATH .. ";C:\\tools\\ripgrep\\bin"
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -18,6 +20,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+
+-- Global Settings
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -38,3 +42,41 @@ require("lazy").setup({
 	-- automatically check for plugin updates
 	checker = { enabled = true, notify = true },
 })
+
+-- virtual_text
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "",
+		spacing = 4,
+		source = false,
+	},
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "󰌵",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+	},
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = true,
+		header = "",
+		prefix = "",
+	},
+})
+
+vim.o.updatetime = 300
+vim.cmd([[
+    augroup ShowDiagnosticsOnHover
+        autocmd!
+        autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+    augroup END
+]])
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { link = "Comment" })
