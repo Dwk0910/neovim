53faf16ded17619c4c9401e9b481a4546cbe3901
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-25 06:48:19 +0900

    Feature: Added keymaps for packge-info

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -1,9 +1,15 @@
+local mapKey = require("utils.KeyMapper").mapKey
+
 return {
 	"vuki656/package-info.nvim",
 	dependencies = { "MunifTanjim/nui.nvim" },
 	config = function()
-		require("package-info").setup({
+		local package_info = require("package-info")
+		package_info.setup({
 			package_manager = "npm", -- npm, yarn, pnpm
 		})
+
+		mapKey("<leader>nt", package_info.toggle, "n", { silent = true, noremap = true })
+		mapKey("<leader>nn", package_info.change_version, "n", { silent = true, noremap = true })
 	end,
 }
