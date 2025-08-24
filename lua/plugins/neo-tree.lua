030c3cb52061b5d5e9bec161c44654e6cb3df811
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-08-24 14:30:53 +0900

    Style: Cleaning theme-settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,9 +10,26 @@ return {
 			"nvim-tree/nvim-web-devicons", -- optional, but recommended
 		},
 		lazy = false, -- neo-tree will lazily load itself
+		opts = {
+			window = {
+				float = {
+					enable = true,
+					position = "center",
+					border = "rounded",
+				},
+			},
+		},
 		config = function()
 			require("neo-tree").setup({
 				default_component_configs = {
+					indent = {
+						padding = 1,
+						indent_size = 2,
+						with_markers = true,
+						indent_marker = "│",
+						last_indent_marker = "└",
+						highlight = "NeoTreeIndentMarker",
+					},
 					git_status = {
 						symbols = {
 							added = "",
@@ -27,8 +44,18 @@ return {
 						},
 					},
 				},
+				filesystem = {
+					auto_open = false,
+					follow_current_file = true,
+					filtered_items = {
+						visible = true,
+						hide_dotfiles = false,
+						hide_hidden = false,
+					},
+				},
 			})
-			mapKey("<leader>e", ":Neotree toggle<CR>", "n", { silent = true })
+
+			mapKey("<leader>e", ":Neotree float toggle<CR>", "n", { silent = true })
 		end,
 	},
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,28 +1,32 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify",
-    },
-    enabled = true,
-    config = function()
-        require("noice").setup({
-          presets = {
-            command_palette = {
-              views = {
-                cmdline_popup = {
-                  position = {
-                    row = "10%", -- Vertical position (e.g., center)
-                    col = "50%", -- Horizontal position (e.g., center)
-                  },
-                  -- Other size and style options...
-                },
-              },
-            },
-          },
-        })
-    end
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				background_colour = "#000000",
+			},
+		},
+	},
+	enabled = true,
+	config = function()
+		require("noice").setup({
+			presets = {
+				command_palette = {
+					views = {
+						cmdline_popup = {
+							position = {
+								row = "10%", -- Vertical position (e.g., center)
+								col = "50%", -- Horizontal position (e.g., center)
+							},
+							-- Other size and style options...
+						},
+					},
+				},
+			},
+		})
+	end,
 }
-
